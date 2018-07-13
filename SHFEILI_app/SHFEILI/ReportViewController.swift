c84d8bfe0e3dd04e6049d911c3e0f60c92b40ab9
Nemo <tansijie@umich.edu>
2018-07-13 16:32:25 +0800

    first commit

---
+++ SHFEILI_app/SHFEILI/ReportViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ReportViewController.swift
+//  SHFEILI
+//
+//  Created by Nemo on 7/11/18.
+//  Copyright © 2018 Sijie Tan. All rights reserved.
+//
+
+
+import UIKit
+
+class ReportViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
