db02990542c32ad96c081f81f3620b7ebe079897
xinyuhu <xinyuhu@umich.edu>
2018-07-17 16:20:54 +0800

    add machine status

--- SHFEILI_app/SHFEILI/MonitorViewController.swift
+++ SHFEILI_app/SHFEILI/MonitorViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MonitorViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let list = ["machine 1", "machine 2", "machine 3"]
+    let identities = ["m1", "m2", "m3"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count
@@ -19,6 +20,12 @@ class MonitorViewController: UIViewController, UITableViewDataSource, UITableVie
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "machine")
         cell.textLabel?.text = list[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vcName = identities[indexPath.row]
+        let viewController = storyboard?.instantiateViewController(withIdentifier: vcName)
+        self.navigationController?.pushViewController(viewController!, animated: true)
     }
     
 
@@ -32,6 +39,7 @@ class MonitorViewController: UIViewController, UITableViewDataSource, UITableVie
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     
     
