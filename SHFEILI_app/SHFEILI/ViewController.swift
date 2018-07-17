ebb984b43ae9de4ade39af17bd271935b9e109ab
xinyuhu <xinyuhu@umich.edu>
2018-07-17 19:57:03 +0800

    prototyping

--- SHFEILI_app/SHFEILI/ViewController.swift
+++ SHFEILI_app/SHFEILI/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressBar.maxValue = 100
+        progressBar.innerRingColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        progressBar.startProgress(to: 100, duration: 20.0)
+    }
+    
+    @IBAction func stopProgress(_ sender: Any) {
+        progressBar.pauseProgress()
+    }
     
 }
 
